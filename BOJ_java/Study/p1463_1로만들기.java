216ab1cbcd38d4f5de81693c040a85156b1f191f
HoJun Choi <nyhj962@gmail.com>
2022-07-31 16:24:25 +0900

    22/07/31

--- BOJ_java/Study/p1463_1로만들기.java
+++ BOJ_java/Study/p1463_1로만들기.java
@@ -5,28 +5,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class p1463_1로만들기 {
-    static int n, count;
+    static int[] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        n = Integer.parseInt(br.readLine());
-        count = 0;
-        div();
+        int N = Integer.parseInt(br.readLine());
+        dp = new int[N + 1];
+
+        div(N);
+        System.out.println(dp[N]);
     }
 
-    public static void div(){
-        if(n == 1){
-            return;
+    static void div(int n) {
+        dp[1] = 0;
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i - 1] + 1;
+            if(i % 2 == 0){
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            }
+            if(i % 3 == 0){
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            }
         }
-        int rem = n % 3;
-        if(rem == 0){
-            n /= 3;
-        }else if(rem == 1){
-            n-= 1;
-        }else{
-            n /= 2;
-        }
-        count++;
-        div();
     }
 }
---
+++ BOJ_java/Study/p1620_포켓몬마스터/Main.java
@@ -0,0 +1,43 @@
+package Study.p1620_포켓몬마스터;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        String names[] = new String[N + 1];
+        HashMap<String, Integer> nameMap = new HashMap<>();
+
+        String name;
+        for(int i = 1; i <= N; i++){
+            name =  br.readLine();
+            names[i] = name;
+            nameMap.put(name, i);
+        }
+
+        int boundary = 'A';
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < M; i++){
+            String question = br.readLine();
+            if(question.charAt(0) >= boundary) {
+                sb.append(nameMap.get(question) + "\n");
+            } else{
+                int idx = Integer.parseInt(question);
+                sb.append(names[idx] + "\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
