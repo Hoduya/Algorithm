fe558b983535abc25f9ce3b0dc35097eadaa52c6
HoJun Choi <nyhj962@gmail.com>
2022-08-01 21:07:36 +0900

    22/08/01

---
+++ BOJ_java/Study/p11279_최대힙/Main.java
@@ -0,0 +1,32 @@
+package Study.p11279_최대힙;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<Integer> pqMax = new PriorityQueue<>(Collections.reverseOrder());
+
+        int N = Integer.parseInt(br.readLine());
+        int num;
+        while (0 < N--) {
+            num = Integer.parseInt(br.readLine());
+            if(num == 0){
+                if(pqMax.isEmpty()) sb.append("0" + "\n");
+                else sb.append(pqMax.poll() + "\n");
+            }
+            else {
+                pqMax.add(num);
+            }
+        }
+        System.out.println(sb);
+    }
+}
---
+++ BOJ_java/Study/p9095_123더하기/Main.java
@@ -0,0 +1,30 @@
+package Study.p9095_123더하기;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] dp = new int[10 + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+
+        for(int i = 4; i <= 10; i++){
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+
+        int TC = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        while (0 < TC--) {
+            int num = Integer.parseInt(br.readLine());
+            sb.append(dp[num] + "\n");
+        }
+        System.out.println(sb);
+    }
+}
