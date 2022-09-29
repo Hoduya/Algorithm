eba7c8cc376630bd3da272f15c0dcbe124bf8b3c
HoJun Choi <nyhj962@gmail.com>
2022-09-29 23:27:55 +0900

    22/09/29

--- SwiftPS/SwiftPS/Level1/문자열내림차순으로배치하기.swift
+++ SwiftPS/SwiftPS/Level1/문자열내림차순으로배치하기.swift
@@ -10,4 +10,3 @@ import Foundation
 func solution(_ s:String) -> String {
     return String(Array(s).sorted(by: > ))
 }
-
--- SwiftPS/SwiftPS/Level1/시저암호.swift
+++ SwiftPS/SwiftPS/Level1/시저암호.swift
@@ -8,5 +8,20 @@
 import Foundation
 
 func solution(_ s:String, _ n:Int) -> String {
-    return ''
+    var answer = ""
+    let A = Character("A").asciiValue!
+    let a = Character("a").asciiValue!
+    
+    for char in s {
+        var ascii = char.asciiValue!
+        if char.isUppercase {
+            ascii = (ascii - A + UInt8(n)) % 26 + A
+        } else if char.isLowercase {
+            ascii = (ascii - a + UInt8(n)) % 26 + a
+        }
+        answer += String(UnicodeScalar(ascii))
+    }
+    return answer
 }
+
+print(solution("AB z", 1))
