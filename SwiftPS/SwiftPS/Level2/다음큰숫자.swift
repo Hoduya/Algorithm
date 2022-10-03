149e2b4cd6a7f4e701fe7bf4f1f33fe8ff7f1cc6
HoJun Choi <nyhj962@gmail.com>
2022-10-03 15:40:04 +0900

    22/10/03

--- SwiftPS/SwiftPS/Level2/다음큰숫자.swift
+++ SwiftPS/SwiftPS/Level2/다음큰숫자.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+func solution(_ n:Int) -> Int
+{
+    var num = n
+    let ones = String(num, radix: 2).reduce(0){ $0 + ($1 == "1" ? 1 : 0) }
+    var oneCount = 0
+    
+    while(ones != oneCount) {
+        num += 1
+        oneCount = String(num, radix: 2).reduce(0){ $0 + ($1 == "1" ? 1 : 0) }
+    }
+    
+    return num
+}
--- SwiftPS/SwiftPS/main.swift
+++ SwiftPS/SwiftPS/main.swift
@@ -1,25 +1,35 @@
 import Foundation
 
-func solution(_ s:String) -> [Int] {
-    var str = s
-    str.removeLast(2)
-    str.removeFirst(2)
+func solution(_ n:Int, _ left:Int64, _ right:Int64) -> [Int] {
+    let left = Int(left)
+    let right = Int(right)
+    
     
     var answer = [Int]()
-    var arr = str.components(separatedBy: "},{")
-    arr.sort{ $0.count < $1.count }
-
-    var set = Set<Int>()
-    for string in arr {
-        let nums = string.split(separator: ",")
-        for string in nums {
-            if set.insert(Int(string)!).inserted {
-                answer.append(Int(string)!)
-            }
-        }
+    
+    for i in left...right {
+        let row = i / n + 1
+        let col = i % n + 1
+        answer.append(row > col ? row : col)
     }
+    
     return answer
 }
-
-print(solution("{{20,111},{111}}"))
-
+print(solution(3, 0, 8))
+/*
+    1 2 3 4
+    2 2 3 4
+    3 3 3 4
+    4 4 4 4
+    
+    1 2 3 4 2 2 3 4 3 3 3  4 4 4 4
+    0 1 2 3 4 5 6 7 8 9 10 11
+    
+    i 번째 요소는 (i % n),
+    단 (i % n) < ((i / n) + 1) 일 경우 (i / n) + 1
+ 
+    i 는
+    1 2 3 2 2 3 3 3
+    
+    
+ */
